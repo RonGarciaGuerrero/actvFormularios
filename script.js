f82f8c355be5b6cb6118296d00dc9ac0a368fe4d
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-11-18 14:53:59 +0100

    primer y segundo ejercicio hecho

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 //con addEventListener
-
-document.getElementById("nombre").addEventListener('blur', function (){ var x = document.getElementById("nombre");
+//alert("se esta ejecutando el javaScript");
+function listenerMayusculas (evento){ 
+    //var x = document.getElementById("nombre");
+    var x = evento.currentTarget; ///el currentTarget del evento devuelve el elemento en el que se produjo el evento (al que se le asocio el listener)
+    //Returns the element whose event listeners triggered the event
     x.value = x.value.toUpperCase();
-}, false)
+}
+function asociarListeners(loadEvent){//dentro de esta funcion hay que meter todo el codigo que debe esperar a que se cargue la ventana entera para ejecutarse
+    //alert('hola');
+    document.getElementById("nombre").addEventListener('blur', listenerMayusculas, false);
+    document.getElementById("apellidos").addEventListener('blur', listenerMayusculas, false);
+    
+}
+
+
+//como el script esta metido en el head se ejecuta antes de cargar todo el documento html por lo que no consigue el elemento cuyo id es nombre, por eso en la consola daba el error de acceder a la propiedad addEventListener de null
+
+//la solucion a esto es ejecutar este codigo en el evento load del window
+//cuando LA VENTANA  (window) se haya terminado de cargar correctamente entonces se ejecutará la funcion asociar listeners
+window.addEventListener('load',asociarListeners,false);
+
 
 
 //sin el addEventListener
