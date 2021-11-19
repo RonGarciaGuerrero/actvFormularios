c75567e6cf4b10ffc778cfdc4ba168c2390afeb7
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-11-19 12:50:27 +0100

    hasta el 3 completo

--- script.js
+++ script.js
@@ -6,11 +6,52 @@ function listenerMayusculas (evento){
     //Returns the element whose event listeners triggered the event
     x.value = x.value.toUpperCase();
 }
+
+function validarNombre (evento){
+    var x = evento.currentTarget;
+    var regex = /(^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,16})+$/;
+    //var correcto=true;
+    //alert(x.value);
+    if (!regex.test(x.value)){
+        //correcto=false;
+        document.getElementById("errores").innerHTML="<p>El nombre es inválido</p>";
+        document.getElementById("nombre").focus();//pongo el foco en el elemento que esta erroneo
+    }else{
+        document.getElementById("errores").innerHTML="";
+    }
+}
+function validarApellidos (evento){
+    var x = evento.currentTarget;
+    var regex = /(^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,16})+$/;
+    if (!regex.test(x.value)){
+        document.getElementById("errores").innerHTML="<p>Los apellidos son inválidos</p>";
+        document.getElementById("apellidos").focus();
+    }else{
+        document.getElementById("errores").innerHTML="";
+    }
+
+}
+function validarEdad(evento){
+    var x = evento.currentTarget;
+    var regex = /^[0-9]$|^[1-9][0-9]$|^(105)$/;//solo acepta numeros entre 0 y 105
+    
+    //if (x.value<0 || x.value>105){
+    if (!regex.test(x.value)){
+        document.getElementById("errores").innerHTML="<p>La edad es inválida</p>";
+        document.getElementById("edad").focus();
+    }else{
+        document.getElementById("errores").innerHTML="";
+    }
+}
+
 function asociarListeners(loadEvent){//dentro de esta funcion hay que meter todo el codigo que debe esperar a que se cargue la ventana entera para ejecutarse
     //alert('hola');
     document.getElementById("nombre").addEventListener('blur', listenerMayusculas, false);
     document.getElementById("apellidos").addEventListener('blur', listenerMayusculas, false);
-    
+    document.getElementById("nombre").addEventListener('blur',validarNombre, false);
+    document.getElementById("apellidos").addEventListener('blur',validarApellidos, false);
+    document.getElementById("edad").addEventListener('blur',validarEdad,false);
+
 }
 
 
@@ -19,6 +60,7 @@ function asociarListeners(loadEvent){//dentro de esta funcion hay que meter todo
 //la solucion a esto es ejecutar este codigo en el evento load del window
 //cuando LA VENTANA  (window) se haya terminado de cargar correctamente entonces se ejecutará la funcion asociar listeners
 window.addEventListener('load',asociarListeners,false);
+
 
 
 
